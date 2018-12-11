b03a9de04d61857fe9b089204585b956259afc98
Tural <osmanov.tural2013@yandex.ru>
2018-12-12 00:29:26 +0300

    тестовый коммит

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Login from "@/components/Auth/Login"
 import Registration from "@/components/Auth/Registration"
 
 Vue.use(Router)
-
+ 
 export default new Router({
   mode: 'history',
   routes: [
